f9a3e0d6ba857ad10b0599a7a094f9942eee378b
av4lyfo <22013834@myrp.edu.sg>
2024-10-25 14:43:57 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+import Movie from './components/Movie';
+
+class AllMovies extends React.Component {
+  render() {
+    return (
+        <ScrollView>
+          <Text></Text>
+          <Text></Text>
+          <Movie title="Doctor Sleep" year="2019" icon_name="skull" poster={require('./img/doctor-sleep.jpg')}/>
+          <Movie title="Midway" year="2020" icon_name="person-rifle" poster={require('./img/midway.jpg')}/>
+
+        </ScrollView>
+    );
+  }
+}
+export default AllMovies;
